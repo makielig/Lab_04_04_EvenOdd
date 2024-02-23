89cecd8222b0408510897ebfb875cce6fef363a8
idema <makielig@mail.uc.edu>
2024-02-23 11:01:25 -0500

    lol

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int numToExamine = 2; // Example number to examine
+
+        // Determine if the number is even or odd using the modulo operator
+        if (numToExamine % 2 == 0) {
+            System.out.println(numToExamine + " is Even.");
+        } else {
+            System.out.println(numToExamine + " is Odd.");
+        }
+    }
+}
